c151b346f221e1334b758411afdeef3467db5369
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-10-07 13:33:52 +0200

    i9305.mk: set audio config file name fixed to i9305, otherweise a GSI won't find it.

--- i9305.mk
+++ i9305.mk
@@ -38,7 +38,7 @@ DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/m3
+    $(LOCAL_PATH)/configs/tiny_hw.xml:vendor/etc/sound/i9305
 
 # Camera
 PRODUCT_PACKAGES += \
