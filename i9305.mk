c6f711fecad7ffa00798d22df0313bbb8e95790b
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-10-07 13:16:09 +0200

    i9305.mk: don't copy com.android.nfc_extras.xml, it will be copied automatically.

--- i9305.mk
+++ i9305.mk
@@ -68,7 +68,6 @@ PRODUCT_PACKAGES += \
     sensors.smdk4x12
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # NFCEE access control
