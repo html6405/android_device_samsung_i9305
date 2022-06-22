c95831e176951f46df718d820cd1ecc50c27e2b7
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-06-22 14:22:47 +0200

    i9305: remove not needed libsecril-client and libsecril-client-sap.

--- i9305.mk
+++ i9305.mk
@@ -60,10 +60,6 @@ PRODUCT_PACKAGES += \
     Tag
 
 # RIL
-PRODUCT_PACKAGES += \
-    libsecril-client \
-    libsecril-client-sap
-
 PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1
 
